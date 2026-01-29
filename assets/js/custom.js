996278974469b778555e337305b0144d2dc85c47
SANTU MAITY <contact@devsantu.in>
2026-01-29 11:11:53 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,110 +1,88 @@
-// top sticky
-var headerHeight = $(".top-navbar").height();
-$(window).scroll(function () {
-    var scrollPos = $(this).scrollTop();
+(function ($) {
+    "use strict";
 
-    if (scrollPos >= headerHeight) {
-        $("header").addClass("sticky");
-    } else {
-        $("header").removeClass("sticky");
-    }
-});
+    $(document).ready(function () {
+        var headerHeight = $(".top-navbar").outerHeight();
 
-// Client Testimonial
-$('#clientTestimonial').owlCarousel({
-    nav: false,
-    dots: true,
-    items: 1,
-    loop: true,
-    margin: 70,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplaySpeed: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
+        $(window).on("scroll", function () {
+            var scrollPos = $(this).scrollTop();
+
+            if (scrollPos >= headerHeight) {
+                $("header").addClass("sticky");
+            } else {
+                $("header").removeClass("sticky");
+            }
+        });
+
+        $("#clientTestimonial").owlCarousel({
+            nav: false,
+            dots: true,
             items: 1,
-        },
-        600: {
-            items: 1,
-        },
-        1000: {
-            items: 1
-        },
-        1200: {
-            items: 1
-        }
-    }
-});
+            loop: true,
+            margin: 70,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplaySpeed: 5000,
+            autoplayHoverPause: true
+        });
 
-// what they says
-$('#whatThetSays').owlCarousel({
-    nav: false,
-    dots: true,
-    items: 1,
-    loop: true,
-    margin: 70,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplaySpeed: 5000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
+        $("#whatThetSays").owlCarousel({
+            nav: false,
+            dots: true,
             items: 1,
-        },
-        600: {
-            items: 1,
-        },
-        1000: {
-            items: 1
-        },
-        1200: {
-            items: 1
-        }
-    }
-});
+            loop: true,
+            margin: 70,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplaySpeed: 5000,
+            autoplayHoverPause: true
+        });
 
-// Our blog slider
-$('#ourBlogItem').owlCarousel({
-    nav: true,
-    dots: false,
-    items: 1,
-    loop: true,
-    margin: 70,
-    autoplay: false,
-    autoplayTimeout: 4500,
-    autoplaySpeed: 4500,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 1,
-        },
-        1000: {
-            items: 2
-        },
-        1200: {
-            items: 2
-        }
-    }
-});
+        $("#ourBlogItem").owlCarousel({
+            nav: true,
+            dots: false,
+            loop: true,
+            margin: 70,
+            autoplay: false,
+            autoplayTimeout: 4500,
+            autoplaySpeed: 4500,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                1000: {
+                    items: 2
+                }
+            }
+        });
 
-// Accordian js
-$(document).ready(function () {
-    $(".accordion-list > li > .answer").hide();
+        $(".accordion-list > li > .answer").hide();
 
-    $(".accordion-list > li:first").addClass("active").find(".answer").show();
+        $(".accordion-list > li:first")
+            .addClass("active")
+            .find(".answer")
+            .show();
 
-    $(".accordion-list > li").click(function () {
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active").find(".answer").slideUp();
-        } else {
-            $(".accordion-list > li.active .answer").slideUp();
-            $(".accordion-list > li.active").removeClass("active");
-            $(this).addClass("active").find(".answer").slideDown();
-        }
-        return false;
+        $(".accordion-list > li").on("click", function (e) {
+            e.preventDefault();
+
+            if ($(this).hasClass("active")) {
+                $(this)
+                    .removeClass("active")
+                    .find(".answer")
+                    .slideUp();
+            } else {
+                $(".accordion-list > li.active")
+                    .removeClass("active")
+                    .find(".answer")
+                    .slideUp();
+
+                $(this)
+                    .addClass("active")
+                    .find(".answer")
+                    .slideDown();
+            }
+        });
     });
-});
+})(jQuery);
