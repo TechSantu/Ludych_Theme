a5ab96a26ba164bfeb9520a8ab6099eca8a24eab
TechSantu <contact@devsantu.in>
2026-02-13 15:35:02 +0530

    feat: Implement new 'Packages' page with a dedicated PHP template and styling adapted from index5.html.

--- templates/packages.php
+++ templates/packages.php
@@ -11,6 +11,11 @@ $post_id = get_the_ID();
 
 get_header();
 
+// Include Google Fonts (Inria Serif & Roboto)
+echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
+echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
+echo '<link href="https://fonts.googleapis.com/css2?family=Inria+Serif:wght@400;700&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">';
+
 // Include Packages CSS
 echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/assets/css/packages.css">';
 
